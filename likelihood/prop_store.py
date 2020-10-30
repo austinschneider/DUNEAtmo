0748b360cd7f4238f19695966d673200b097cefc
Austin Schneider <hogenshpogen@gmail.com>
2020-10-30 14:19:46 -0400

    Fix cache bugs. Account for implicit dependence

--- likelihood/prop_store.py
+++ likelihood/prop_store.py
@@ -9,11 +9,12 @@ class memodict_(collections.OrderedDict):
         self.f = f
         self.maxsize = maxsize
     def __getitem__(self, key, extra):
-        if key in super():
+        if super().__contains__(key):
             return super().__getitem__(key)
         if len(self) == self.maxsize:
             self.popitem(last=False)
-        ret = super()[key] = self.f(key, extra)
+        ret = self.f(key, extra)
+        super().__setitem__(key, ret)
         return ret
     def __call__(self, key, extra):
         return self.__getitem__(key, extra)
@@ -26,6 +27,8 @@ def memodict(f, maxsize=1):
 class store_entry:
     def __init__(self, name, dependents, atomic_operation):
         self.name = name
+        if dependents is None:
+            dependents = []
         self.dependents = dependents
         self.atomic_operation = atomic_operation
     def __call__(self, *args):
@@ -33,7 +36,7 @@ class store_entry:
 
 class store_initialized_entry(store_entry):
     def __init__(self, uninitialized, the_store, physical_props, props, cache_size=None):
-        store_entry.__init__(uninitialized.name, uninitialized.dependents, uninitialized.atomic_operation)
+        store_entry.__init__(self, uninitialized.name, uninitialized.dependents, uninitialized.atomic_operation)
 
         self.the_store = the_store
 
@@ -41,8 +44,11 @@ class store_initialized_entry(store_entry):
             cache_size = 1
         self.cache_size = cache_size
 
+        self.implicit_physical_props = []
         self.physical_props = []
         self.props = []
+        self.physical_props_indices = []
+        self.props_indices = []
         self.is_physical = []
         for i,name in enumerate(self.dependents):
             if name in physical_props:
@@ -67,7 +73,8 @@ class store_initialized_entry(store_entry):
         return self.atomic_operation(*values)
 
     def __call__(self, physical_parameters):
-        these_params = [physical_parameters[k] for k in self.physical_props]
+        these_params  = tuple([physical_parameters[k] for k in self.physical_props])
+        these_params += tuple([physical_parameters[k] for k in self.implicit_physical_props])
         return self.cache(these_params, physical_parameters)
 
 class store:
@@ -77,7 +84,9 @@ class store:
         self.cache_sizes = dict()
         self.initialized_props = dict()
 
-    def get_prop(self, name, physical_parameters):
+    def get_prop(self, name, physical_parameters=None):
+        if physical_parameters is None:
+            physical_parameters = dict()
         return self.initialized_props[name](physical_parameters)
 
     def add_prop(self, name, dependents, atomic_operation, cache_size=None):
@@ -85,8 +94,10 @@ class store:
         self.props[name] = prop
         self.cache_sizes[name] = cache_size
 
-    def initialize(self):
-        del self.initialized_props
+    def initialize(self, keep_cache=False):
+        old_initialized_props = self.initialized_props
+        if not keep_cache:
+            del old_initialized_props
         self.initialized_props = dict()
         props = self.props.keys()
         dependents = set()
@@ -98,11 +109,38 @@ class store:
         physical_props = dependents - props
         for prop in props:
             entry = self.props[prop]
-            these_deps = set(entry.dependents)
+            if entry.dependents is not None:
+                these_deps = set(entry.dependents)
+            else:
+                these_deps = set()
             these_physical_props = these_deps - props
             these_props = these_deps - these_physical_props
             initialized_entry = store_initialized_entry(entry, self, these_physical_props, these_props, cache_size=self.cache_sizes[prop])
             self.initialized_props[prop] = initialized_entry
+        if keep_cache:
+            keys = old_initialized_props.keys()
+            for k in keys:
+                if k in self.initialized_props:
+                    self.initialized_props[k].cache = old_initialized_props[k].cache
+            del old_initialized_props
+
+        def add_implicit_dependencies(prop):
+            print("Parent:", prop)
+            initialized_entry = self.initialized_props[prop]
+            if len(initialized_entry.implicit_physical_props) == 0:
+                prop_deps = set()
+                print("Children:")
+                for dprop in initialized_entry.props:
+                    print("\t" + dprop)
+
+                for dprop in initialized_entry.props:
+                    add_implicit_dependencies(dprop)
+                    prop_deps |= set(self.initialized_props[dprop].physical_props)
+                    prop_deps |= set(self.initialized_props[dprop].implicit_physical_props)
+                prop_deps -= set(initialized_entry.physical_props)
+                initialized_entry.implicit_physical_props = list(prop_deps)
+        for prop in props:
+            add_implicit_dependencies(prop)
 
 class store_view:
     def __init__(self, the_store, parameters):
