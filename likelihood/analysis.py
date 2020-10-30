d47dbeae3119e17ddc5ae7c16613261647e89a4c
Austin Schneider <hogenshpogen@gmail.com>
2020-10-30 18:49:17 -0400

    Likelihood and comments

--- likelihood/analysis.py
+++ likelihood/analysis.py
@@ -1,6 +1,7 @@
 import os
 import os.path
 import collections
+import functools
 import numpy as np
 import nuflux
 import nuSQUIDSpy as nsq
@@ -24,7 +25,47 @@ import prop_store
 
 the_store = prop_store.store()
 
-# How to load the mc
+### How to build an analysis
+## Instantiate the store
+#
+# the_store = prop_store.store()
+#
+## If you need a computed value, you go to the store:
+#
+# my_expensive_numbers = the_store.get_prop('expensive_numbers', physics_parameters)
+#
+## But first you have to tell the store how to do things.
+##  So you define a function:
+#
+# def my_func(foo, bar):
+#     ret = ...
+#     ... compute things ...
+#     return ret
+#
+## Register the function with the store:
+##  This requires a name for the output of the function,
+##  the ordered names of parameters it depends on,
+##  and the function itself.
+#
+# the_store.add_prop('my_value', ["value_of_foo", "value_of_bar"], my_func)
+#
+## Now you can register other functions that use the output of your other functions
+# def my_other_func(my_value):
+#     return (my_value + 1)/2.0
+#
+## Just please don't define an infinite loop in this way...
+#
+## Finally you have to initialize the store so it can work out the
+##  relationships between all of things you defined and spin up the object
+##  caches
+#
+# the_store.initialize()
+#
+## If the store was already initialized and you want to keep the caches
+
+
+# How to load the mc with precomputed generation probabilities
+# This actually has the binning as well
 def load_mc():
     import data_loader
     import binning
@@ -34,17 +75,19 @@ def load_mc():
     return mc, bin_slices
 the_store.add_prop('sorted_mc', None, load_mc, cache_size=1)
 
-# Bin the mc and sort by
+# Actually get just the mc
 def get_mc(sorted_mc):
     return sorted_mc[0]
 the_store.add_prop('mc', ['sorted_mc'], get_mc, cache_size=1)
 
+# Just get the binning
 def get_binning(sorted_mc):
     return sorted_mc[1]
 the_store.add_prop('mc_binning', ['sorted_mc'], get_binning, cache_size=1)
 the_store.initialize()
 
 # Force loading of the MC
+# So we can look at what is inside
 mc = the_store.get_prop('mc')
 binning = the_store.get_prop('mc_binning')
 
@@ -58,14 +101,18 @@ for name in mc.dtype.names:
     s = str(name)
     f = get_mc_f(s)
     the_store.add_prop('mc_'+s, ['mc'], f, cache_size=1)
+# Now things like "mc_energy", "mc_zenith", etc. are registered with "mc" as a dependent
 
+# Convenience function for calling the nusquids flux repository
+# This nusquids repository does the 3+1 scenario, but the concept is easily extensible
 def nsq_flux(numnu, dm2, th14, th24, th34, cp):
     flux = osc[(numnu, dm2, th14, th24, th34, cp)]
     return flux
 the_store.add_prop('nsq_flux', ['numnu', 'dm2', 'th14', 'th24', 'th34', 'cp'], nsq_flux)
 
+# How to get the flux for MC events from nusquids
+# That is before we modify it with additional parameters
 def baseline_flux_conv(flux, mc_energy, mc_zenith, mc_particle_type):
-    print("baseline_flux_conv")
     res = np.empty(mc_energy.shape)
     mc_nsq_particle_type = np.array(mc_particle_type < 0).astype(int)
     mc_nsq_flavor = (np.abs(mc_particle_type) / 2 - 6).astype(int)
@@ -80,19 +127,84 @@ def baseline_flux_conv(flux, mc_energy, mc_zenith, mc_particle_type):
     return res
 the_store.add_prop('baseline_flux_conv', ['nsq_flux', 'mc_energy', 'mc_zenith', 'mc_particle'], baseline_flux_conv)
 
+# Now we can modify the flux according to cosmic ray uncertainties
 def conv_tilt_correction(mc_energy, CRDeltaGamma, pivot_energy=500):
     return (mc_energy / pivot_energy) ** -CRDeltaGamma
 the_store.add_prop('conv_tilt_correction', ['mc_energy', 'CRDeltaGamma'], conv_tilt_correction)
 
+# Combining the baseline flux and the cosmic ray correction
 def conv_flux_tilt_corrected(baseline_flux_conv, conv_tilt_correction):
     return baseline_flux_conv * conv_tilt_correction
 the_store.add_prop('conv_flux_tilt_corrected', ['baseline_flux_conv', 'conv_tilt_correction'], conv_flux_tilt_corrected)
 
+# Changing the conventional normalization
 def flux_conv(convNorm, conv_flux_tilt_corrected):
     return convNorm * conv_flux_tilt_corrected
 the_store.add_prop('flux_conv', ["convNorm", "conv_flux_tilt_corrected"], flux_conv)
 
+# Just the sample livetime
+def get_livetime():
+    return 365.25*24*3600 * 9
+the_store.add_prop('livetime', None, get_livetime)
+
+# Combine the livetime and generation probability
+# We do this because neither will ever change
+def livetime_gen_prob(livetime, mc_gen_prob):
+    return livetime / mc_gen_prob
+the_store.add_prop('livetime_gen_prob', ['livetime', 'mc_gen_prob'], livetime_gen_prob)
+
+# Compute the weight of MC events
+def mc_weight(flux_conv, livetime_gen_prob):
+    return flux_conv * livetime_gen_prob
+the_store.add_prop('mc_weight', ['flux_conv', 'livetime_gen_prob'], mc_weight)
+
+# Bin the weights
+def binned_mc_weight(mc_weight, mc_binning):
+    return [mc_weight[b] for b in mc_binning]
+the_store.add_prop('binned_mc_weight', ['mc_weight', 'mc_binning'], binned_mc_weight)
+
+# Compute the MC expectation per bin
+def expect(binned_mc_weight):
+    return [np.sum(w) for w in binned_mc_weight]
+the_store.add_prop('expect', ['binned_mc_weight'], expect)
+
+# Compute the MC square expectation per bin
+def expect_sq(binned_mc_weight):
+    return [np.sum(w**2) for w in binned_mc_weight]
+the_store.add_prop('expect_sq', ['binned_mc_weight'], expect_sq)
+
+# Compute the weights for an asimov dataset
+# This is kept separate so that it has its own cache
+def asimov_data(mc_weight):
+    return mc_weight
+the_store.add_prop('asimov_data', ['mc_weight'], asimov_data)
+
+def binned_asimov_data(asimov_data, mc_binning):
+    return [asimov_data[b] for b in mc_binning]
+the_store.add_prop('binned_asimov_data', ['asimov_data', 'mc_binning'], binned_asimov_data)
+
+def asimov_expect(binned_asimov_data):
+    return [np.sum(w) for w in binned_asimov_data]
+the_store.add_prop('asimov_expect', ['binned_asimov_data'], asimov_expect)
+
+def asimov_expect_sq(binned_asimov_data):
+    return [np.sum(w**2) for w in binned_asimov_data]
+the_store.add_prop('asimov_expect_sq', ['binned_asimov_data'], asimov_expect_sq)
+
+# Spin up the caches
 the_store.initialize(keep_cache=True)
+
+# Now we can define the likelihood
+# For now we are ignoring the fact that we could have data (asimov only)
+import likelihood
+def asimov_binned_likelihood(parameters, asimov_parameters):
+    asimov_expect = the_store.get_prop('asimov_expect', asimov_parameters)
+    expect = the_store.get_prop('expect', parameters)
+    expect_sq = the_store.get_prop('expect_sq', parameters)
+    return likelihood.LEff(asimov_expect, expect, expect_sq)
+
+def asimov_likelihood(parameters, asimov_parameters):
+    return np.sum(asimov_binned_likelihood(parameters, asimov_parameters))
 
 dm2 = 1.
 th14 = 0.
@@ -111,5 +223,19 @@ physical_params = {
         "CRDeltaGamma": 0.0,
         }
 
-the_store.get_prop("flux_conv", physical_params)
+asimov_params = {
+        'numnu': 3,
+        "dm2": 0.0,
+        "th14": 0.0,
+        "th24": 0.0,
+        "th34": 0.0,
+        "cp": 0.0,
+        "convNorm": 1.0,
+        "CRDeltaGamma": 0.0,
+        }
+
+# Compute the delta LLH between the null (3 neutrino) and an alternative (3+1) scenario
+print(asimov_likelihood(asimov_params, asimov_params) - asimov_likelihood(physical_params, asimov_params))
+
+# Now you can hook this up to a minimizer, etc. ...
 
