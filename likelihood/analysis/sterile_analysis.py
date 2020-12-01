b64541631e2a5b92da91982e3f55189b4a484e63
Austin Schneider <hogenshpogen@gmail.com>
2020-12-01 15:20:09 -0500

    Get the right imports

--- likelihood/analysis/sterile_analysis.py
+++ likelihood/analysis/sterile_analysis.py
@@ -1,5 +1,9 @@
+import sys
 import os
 import os.path
+base_path = os.environ['GOLEMSPACE']
+core_path = base_path + '/sources/DUNEAtmo/likelihood/core/'
+sys.path.insert(0, core_path)
 import collections
 import functools
 import numpy as np
