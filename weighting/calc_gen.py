d912f0e5dbab978c7e0a86a759100a08dc329ee4
Austin Schneider <hogenshpogen@gmail.com>
2020-12-07 17:33:28 -0500

    Weighing code from bedrock study

--- weighting/calc_gen.py
+++ weighting/calc_gen.py
@@ -1,154 +1,247 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Calculate generation probabilities")
+parser.add_argument('--config',
+        type=str,
+        dest='config',
+        required=True,
+        )
+parser.add_argument('--propagated',
+        type=str,
+        dest='propagated',
+        required=True,
+        )
+parser.add_argument('--lic',
+        type=str,
+        dest='lic',
+        required=True,
+        )
+parser.add_argument('--outdir',
+        type=str,
+        dest='outdir',
+        required=True,
+        )
+parser.add_argument('--output',
+        type=str,
+        dest='output',
+        required=True,
+        )
+args = parser.parse_args()
+
+import os
+import os.path
 import numpy as np
 import LWpy
 import LeptonInjector
 import json
+import glob
+base_path = os.environ['GOLEMSPACE']
 
-outdir = './plots/oscillations_alt/'
-
-s = LWpy.read_stream('./injected/config_DUNE.lic')
-blocks = s.read()
-earth_model_params = [
+gen_earth_model_params = [
     "DUNE",
-    "../LWpy/LWpy/resources/earthparams/",
+    base_path + "/sources/LWpy/LWpy/resources/earthparams/",
     ["PREM_dune"],
     ["Standard"],
     "NoIce",
     20.0*LeptonInjector.Constants.degrees,
     1480.0*LeptonInjector.Constants.m]
 
-generators = []
-for block in blocks:
-    block_name, block_version, _ = block
-    if block_name == 'EnumDef':
+lic_files = sorted(glob.glob(args.lic))
+prop_files = sorted(glob.glob(args.propagated))
+
+by_prefix = dict()
+for lic in lic_files:
+    base = os.path.basename(lic)
+    if not base.endswith('.lic'):
         continue
-    elif block_name == 'VolumeInjectionConfiguration':
-        gen = LWpy.volume_generator(block)
-    elif block_name == 'RangedInjectionConfiguration':
-        gen = LWpy.ranged_generator(block, earth_model_params)
-    else:
-        raise ValueError("Unrecognized block! " + block_name)
-    generators.append(gen)
+    prefix = base[:-len('.lic')]
+    by_prefix[prefix] = lic
 
-json_data = json.load(open('./propagated/propagated.json', 'r'))
-energy = np.array(json_data["energy"])
-zenith = np.array(json_data["zenith"])
-azimuth = np.array(json_data["azimuth"])
-bjorken_x = np.array(json_data["bjorken_x"])
-bjorken_y = np.array(json_data["bjorken_y"])
-final_type_0 = np.array(json_data["final_type_0"]).astype(int)
-final_type_1 = np.array(json_data["final_type_1"]).astype(int)
-particle = np.array(json_data["particle"]).astype(int)
-x = np.array(json_data["x"])
-y = np.array(json_data["y"])
-z = np.array(json_data["z"])
-total_column_depth = np.array(json_data["total_column_depth"])
+print(len(by_prefix))
 
-props = np.empty(len(energy), dtype=[
-      ("energy", energy.dtype),
-      ("zenith", zenith.dtype),
-      ("azimuth", azimuth.dtype),
-      ("bjorken_x", bjorken_x.dtype),
-      ("bjorken_y", bjorken_y.dtype),
-      ("final_type_0", final_type_0.dtype),
-      ("final_type_1", final_type_1.dtype),
-      ("particle", particle.dtype),
-      ("x", x.dtype),
-      ("y", y.dtype),
-      ("z", z.dtype),
-      ("total_column_depth", total_column_depth.dtype),
-      ])
+joint_by_prefix = dict()
+for prop in prop_files:
+    base = os.path.basename(prop)
+    if not base.endswith('.json'):
+        continue
+    prefix = base[:-len('.json')]
+    if prefix not in by_prefix:
+        continue
+    joint_by_prefix[prefix] = (by_prefix[prefix], prop)
 
-props["energy"] = energy
-props["zenith"] = zenith
-props["azimuth"] = azimuth
-props["bjorken_x"] = bjorken_x
-props["bjorken_y"] = bjorken_y
-props["final_type_0"] = final_type_0
-props["final_type_1"] = final_type_1
-props["particle"] = particle
-props["x"] = x
-props["y"] = y
-props["z"] = z
-props["total_column_depth"] = total_column_depth
-def print_stats(name, v, z):
-    return
-    m0 = np.cos(z) < 0
-    m1 = np.cos(z) > 0
-    v0 = v[m0]
-    v1 = v[m1]
-    v_min0 = np.amin(v0)
-    v_max0 = np.amax(v0)
-    v_mean0 = np.sum(v0)/len(v0)
-    v_stddev0 = np.sqrt(np.sum((v0-v_mean0)**2)/len(v0))
+print(len(joint_by_prefix))
 
-    v_min1 = np.amin(v1)
-    v_max1 = np.amax(v1)
-    v_mean1 = np.sum(v1)/len(v1)
-    v_stddev1 = np.sqrt(np.sum((v1-v_mean1)**2)/len(v1))
-    print(name + ":")
-    print("\t"+"min:", v_min0, v_min1)
-    print("\t"+"max:", v_max0, v_max1)
-    print("\t"+"mean:", v_mean0, v_mean1)
-    print("\t"+"stddev:", v_stddev0, v_stddev1)
-    print()
+def load_gen(fname):
+    s = LWpy.read_stream(fname, spline_dir='./splines/')
+    blocks = s.read()
+    return blocks
+
+def blocks_to_gen(blocks):
+    generators = []
+    for block in blocks:
+        block_name, block_version, _ = block
+        if block_name == 'EnumDef':
+            continue
+        elif block_name == 'VolumeInjectionConfiguration':
+            gen = LWpy.volume_generator(block, spline_dir='./splines/')
+        elif block_name == 'RangedInjectionConfiguration':
+            gen = LWpy.ranged_generator(block, gen_earth_model_params, spline_dir='./splines')
+        else:
+            raise ValueError("Unrecognized block! " + block_name)
+        generators.append(gen)
+    return generators
+
+def count_gen_events(blocks):
+    n = 0
+    for block in blocks:
+        block_name, block_version, block_data = block
+        if block_name == 'VolumeInjectionConfiguration' or block_name == 'RangedInjectionConfiguration':
+            n += block_data["events"]
+    return n
+
+earth_model_params = [
+    "DUNE",
+    base_path + "/sources/LWpy/LWpy/resources/earthparams/",
+    ["PREM_dune"],
+    ["Standard"],
+    "NoIce",
+    20.0*LeptonInjector.Constants.degrees,
+    1480.0*LeptonInjector.Constants.m]
 
 nu_interactions_list = LWpy.get_standard_interactions()
 int_model = LWpy.interaction_model(nu_interactions_list, earth_model_params)
-gen_prob = np.zeros(len(props))
-for i, gen in enumerate(generators):
-    print(gen.block_type)
-    p = gen.prob_final_state(props)
-    print_stats("final state", p, zenith)
-    pp = gen.prob_stat(props)
-    p *= pp
-    #print("stat:", pp)
-    nonzero = p != 0
-    if np.any(nonzero):
-        pp = gen.prob_dir(props[nonzero])
-        print_stats("direction", pp, zenith[nonzero])
-        p[nonzero] *= pp
+
+lic_output = os.path.join(args.outdir, args.output + '.lic')
+
+if os.path.exists(lic_output):
+    blocks = load_gen(lic_output)
+else:
+    all_blocks = []
+    for prefix, (lic, prop) in joint_by_prefix.items():
+        json_data = json.load(open(prop, 'r'))
+        n_prop_events = np.sum(json_data["injector_count"])
+        del json_data
+        blocks = load_gen(lic)
+        n_gen_events = count_gen_events(blocks)
+        if n_prop_events != n_gen_events:
+            continue
+        all_blocks.extend(blocks)
+
+    blocks = LWpy.merge_blocks(all_blocks)
+
+    s = LWpy.write_stream(os.path.join(args.outdir, args.output + '.lic'), spline_dir='./splines/')
+    s.write(blocks)
+
+generators = blocks_to_gen(blocks)
+
+def calc_gen_prob(json_data):
+    energy = np.array(json_data["energy"])
+    zenith = np.array(json_data["zenith"])
+    azimuth = np.array(json_data["azimuth"])
+    bjorken_x = np.array(json_data["bjorken_x"])
+    bjorken_y = np.array(json_data["bjorken_y"])
+    final_type_0 = np.array(json_data["final_type_0"]).astype(int)
+    final_type_1 = np.array(json_data["final_type_1"]).astype(int)
+    particle = np.array(json_data["particle"]).astype(int)
+    x = np.array(json_data["x"])
+    y = np.array(json_data["y"])
+    z = np.array(json_data["z"])
+    total_column_depth = np.array(json_data["total_column_depth"])
+
+    props = np.empty(len(energy), dtype=[
+          ("energy", energy.dtype),
+          ("zenith", zenith.dtype),
+          ("azimuth", azimuth.dtype),
+          ("bjorken_x", bjorken_x.dtype),
+          ("bjorken_y", bjorken_y.dtype),
+          ("final_type_0", final_type_0.dtype),
+          ("final_type_1", final_type_1.dtype),
+          ("particle", particle.dtype),
+          ("x", x.dtype),
+          ("y", y.dtype),
+          ("z", z.dtype),
+          ("total_column_depth", total_column_depth.dtype),
+          ])
+
+    props["energy"] = energy
+    props["zenith"] = zenith
+    props["azimuth"] = azimuth
+    props["bjorken_x"] = bjorken_x
+    props["bjorken_y"] = bjorken_y
+    props["final_type_0"] = final_type_0
+    props["final_type_1"] = final_type_1
+    props["particle"] = particle
+    props["x"] = x
+    props["y"] = y
+    props["z"] = z
+    props["total_column_depth"] = total_column_depth
+
+    gen_prob = np.zeros(len(props))
+    for i, gen in enumerate(generators):
+        p = gen.prob_final_state(props)
+        pp = gen.prob_stat(props)
+        p *= pp
         nonzero = p != 0
-    if np.any(nonzero):
-        pp = gen.prob_e(props[nonzero])
-        print_stats("energy", pp, zenith[nonzero])
-        p[nonzero] *= pp
-        nonzero = p != 0
-    if np.any(nonzero):
-        pp = gen.prob_area(props[nonzero])
-        if type(pp) is float:
-            print("area:", pp)
+        if np.any(nonzero):
+            pp = gen.prob_dir(props[nonzero])
+            p[nonzero] *= pp
+            nonzero = p != 0
+        if np.any(nonzero):
+            pp = gen.prob_e(props[nonzero])
+            p[nonzero] *= pp
+            nonzero = p != 0
+        if np.any(nonzero):
+            pp = gen.prob_area(props[nonzero])
+            p[nonzero] *= pp
+            nonzero = p != 0
+        if np.any(nonzero):
+            pp = gen.prob_pos(props[nonzero])
+            p[nonzero] *= pp
+            nonzero = p != 0
+        if np.any(nonzero):
+            pp = gen.prob_kinematics(props[nonzero])
+            p[nonzero] *= pp
+            nonzero = p != 0
+        if np.any(nonzero):
+            first_pos, last_pos = gen.get_considered_range(props[nonzero])
+            pos_prob = int_model.prob_pos(props[nonzero], first_pos, last_pos)
+            int_prob = int_model.prob_interaction(props[nonzero], first_pos, last_pos)
+            p[nonzero] /= pos_prob * int_prob
+        gen_prob += p
+    k_prob = int_model.prob_kinematics(props)
+    fs_prob = int_model.prob_final_state(props)
+    gen_prob /= k_prob * fs_prob
+    return gen_prob
+
+keep_keys = ['energy', 'zenith', 'azimuth', 'particle', 'mu_energy', 'mu_zenith', 'entry_energy', 'entry_zenith', 'morphology']
+gen_prob = []
+all_json = dict()
+tot_prop_events = 0
+for prefix, (lic, prop) in joint_by_prefix.items():
+    json_data = json.load(open(prop, 'r'))
+    gen_prob.extend(calc_gen_prob(json_data).tolist())
+    n_prop_events = np.sum(json_data["injector_count"])
+    tot_prop_events += n_prop_events
+    for k in json_data.keys():
+        if k not in keep_keys:
+            continue
+        if k not in all_json:
+            all_json[k] = json_data[k]
         else:
-            print_stats("area", pp, zenith[nonzero])
-        p[nonzero] *= pp
-        nonzero = p != 0
-    if np.any(nonzero):
-        pp = gen.prob_pos(props[nonzero])
-        print_stats("pos", pp, zenith[nonzero])
-        p[nonzero] *= pp
-        nonzero = p != 0
-    if np.any(nonzero):
-        pp = gen.prob_kinematics(props[nonzero])
-        print_stats("kinematics", pp, zenith[nonzero])
-        p[nonzero] *= pp
-        nonzero = p != 0
-    if np.any(nonzero):
-        first_pos, last_pos = gen.get_considered_range(props[nonzero])
-        pos_prob = int_model.prob_pos(props[nonzero], first_pos, last_pos)
-        int_prob = int_model.prob_interaction(props[nonzero], first_pos, last_pos)
-        print_stats("physical pos", pos_prob, zenith[nonzero])
-        print_stats("physical int", int_prob, zenith[nonzero])
-        p[nonzero] /= pos_prob * int_prob
-    gen_prob += p
-k_prob = int_model.prob_kinematics(props)
-fs_prob = int_model.prob_final_state(props)
-print_stats("physical kinematics", k_prob, zenith)
-print_stats("physical final state", fs_prob, zenith)
-gen_prob /= k_prob * fs_prob
-print_stats("gen_prob", gen_prob, zenith)
-print_stats("1/gen_prob", 1./gen_prob, zenith)
+            all_json[k].extend(json_data[k])
 
-json_data["gen_prob"] = gen_prob.tolist()
+n_gen_events = count_gen_events(blocks)
+if n_gen_events != tot_prop_events:
+    print("Generated event count doe not match propagated event count!!")
+    print("Generated events:", n_gen_events)
+    print("Propagated events:", tot_prop_events)
+    print("Applying approximate rescaling!")
+    gen_prob = (np.array(gen_prob) * n_gen_events / tot_prop_events).tolist()
+    os.remove(lic_output)
 
-f = open('./weighted/weighted.json', 'w')
+json_data = all_json
+json_data["gen_prob"] = gen_prob
+
+f = open(os.path.join(args.outdir, args.output + '.json'), 'w')
 json.dump(json_data, f)
 f.close()
