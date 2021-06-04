9547a6943fdfcd03e16953d1799940cb8f918d74
Austin Schneider <hogenshpogen@gmail.com>
2021-06-04 12:16:10 -0400

    Flux-gen jobs

---
+++ jobs/fas/flux_pre_gen/gen_lv_sparse.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=inject
+#SBATCH --output=array_%A_%a.out
+#SBATCH --error=array_%A_%a.err
+#SBATCH --array=1-10
+#SBATCH --time=08:00:00
+#SBATCH --partition=arguelles_delgado
+#SBATCH --ntasks=1
+#SBATCH --mem=2G
+#SBATCH --cpus-per-task 1
+
+
+#source /n/holyscratch01/arguelles_delgado_lab/Lab/DUNEAnalysis/programs/LIDUNE/setup.sh
+source /n/home09/aschneider/programs/LIDUNE/setup_local.sh
+export HDF5_USE_FILE_LOCKING=FALSE
+python -u $GOLEMSOURCEPATH/DUNEAtmo/likelihood/flux_pre_gen/gen_lv_sparse.py --chunks 2000 --chunk-number ${SLURM_ARRAY_TASK_ID}
---
+++ jobs/fas/flux_pre_gen/submit_sparse.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sbatch --array=0-1999 gen_lv_sparse.sh
